6d85c92d30bfaa5802581de204fbb2c7f60ab688
domonkosgyomorey <domonkosgyomorey@gmail.com>
2023-06-07 02:27:17 +0200

    Its a bit buggy, but 2D/3D rotations added

--- main.c
+++ main.c
@@ -7,15 +7,80 @@ int main(){
     vec_put(a, (float[]){1, 2, 3}, 3);
     Vec b = vec_alloc(3);
     vec_put(b, (float[]){1, 2, 3}, 3);
+
     printf("Vec 'a': ");
     vec_print(a);
     printf("Vec 'b': ");
     vec_print(b);
+    
     printf("Vec a+b: ");
     vec_print(vec_add(a, b));
+    
+    printf("Vec a-b: ");
+    vec_print(vec_sub(a, b));
+    
     printf("dot: %lf\n", vec_dot(a, b));
+    
     printf("cross: %lf\n", vec_cross(a, b));
+    
     printf("Vec a*10: ");
     vec_print(vec_mul(a, 10));
-    return 0;
+    
+    Vec e = vec_alloc(2);
+    vec_put(e, (float[]){1, 0}, 2);
+    
+    printf("Vec 'e': ");
+    vec_print(e);
+    
+    printf("'e' rotated by 90°: \t\t");
+    vec_print(vec_rotate2d(e, 90));
+    
+    printf("'e' rotated by 180 over P(-1;0) point: \t\t");
+    vec_print(vec_rotate2d_ex(e, -1, 0, 180));;
+    
+    printf("'e' rotated by 45 over P(0;0) point: \t\t");
+    Vec rot_e45 = vec_rotate2d_ex(e, 0, 0, 45);
+    printf(" len=( %lf )\n", vec_len(rot_e45));
+    vec_print(rot_e45);
+
+    Vec third = vec_alloc(3);
+    vec_put(third, (float[]){1, 0, 0}, 3);
+    printf("'third' vec: ");
+    vec_print(third);
+
+    printf("'third' rotated by x:90 y:0 z:0 : \t\t");
+    vec_print(vec_rotate3d(third, 90, 0, 0));
+
+    printf("'third' rotated by x:90 y:90 z:0 : \t\t");
+    vec_print(vec_rotate3d(third, 90, 90, 0));
+  
+    // basicly:
+    // 1st: x:45
+    // 2nd: y:45
+    // 3th: z:45
+    printf("'third' rotated by x:45 y:45 z:45 : \t\t");
+    vec_print(vec_rotate3d(third, 45, 45, 45));
+    
+    // basicly:
+    // 1st y:45
+    // 2nd x:45
+    // 3th z:45      I think its a bug somethere
+    printf("'third' rotated by y:45 x:45 z:45 :\n");
+    Vec y = vec_rotate3d(third, 0, 90, 0);
+    vec_print(y);
+    y = vec_rotate3d(y, 90, 0, 0);
+    vec_print(y);
+    vec_print(vec_rotate3d(y, 0, 0, 90));
+    //vec_print(vec_rotate3d(vec_rotate3d(vec_rotate3d(third, 0, 90, 0), 90, 0, 0), 0, 0, 90));
+
+    printf("'third' rotated by x:0 y:0 z:0 : \t\t");
+    vec_print(vec_rotate3d(third, 0, 0, 0));
+    
+    vec_print(third);
+    printf("'third' rotated by y:90 x:90 z:90 :\n");
+    //y = vec_rotate3d_y(third, 90);
+    //vec_print(y);
+   // y = vec_rotate3d_x(y, 90);
+    //vec_print(y);
+    vec_print(vec_rotate3d_z(third, 90));
 }
--- vec.h
+++ vec.h
@@ -31,7 +31,14 @@
 #define VEC_SIN sin
 #endif
 
+#ifndef VEC_COS
+#include <math.h>
+#define VEC_COS cos
+#endif
+
 #define ll long long int
+#define PI 3.141592653589793
+#define DEG2RAD(D) (D)*(PI/180.0)
 
 typedef struct Vec{
     ll dim;
@@ -42,11 +49,19 @@ Vec vec_alloc(ll dim);
 void vec_print(Vec vec);
 void vec_put(Vec vec, float* data, ll dim);
 Vec vec_add(Vec a, Vec b);
+Vec vec_sub(Vec a, Vec b);
 Vec vec_mul(Vec vec, float scalar);
 double vec_dot(Vec a, Vec b);
 double vec_angle_between(Vec a, Vec b);
 double vec_cross(Vec a, Vec b);
 double vec_len(Vec vec);
+Vec vec_normalize(Vec vec);
+Vec vec_rotate2d(Vec vec, double deg);
+Vec vec_rotate2d_ex(Vec vec, float x, float y, double deg);
+Vec vec_rotate3d(Vec vec, double xdeg,double ydeg, double zdeg);
+Vec vec_rotate3d_x(Vec vec, double xdeg);
+Vec vec_rotate3d_y(Vec vec, double ydeg);
+Vec vec_rotate3d_z(Vec vec, double zdeg);
 
 #ifdef VEC_IMPL
 
@@ -57,7 +72,6 @@ Vec vec_alloc(ll dim){
 }
 
 void vec_print(Vec vec){
-    VEC_PRINT("Vec dim(Vec)=%zu data:\n\t", vec.dim);
     for(ll i = 0; i < vec.dim; i++){
         VEC_PRINT("%.04f\t", vec.data[i]);
     }
@@ -79,6 +93,16 @@ Vec vec_add(Vec a, Vec b){
     }
     return result;
 }
+
+Vec vec_sub(Vec a, Vec b){
+    VEC_ASSERT(a.dim==b.dim && "Doesnt match the dim of the two vector");
+    Vec result = vec_alloc(a.dim);
+    for(ll i = 0; i < a.dim; i++){
+        result.data[i] = a.data[i]-b.data[i];
+    }
+    return result;
+}
+
 
 Vec vec_mul(Vec vec, float scalar){
     Vec result = vec_alloc(vec.dim);
@@ -117,6 +141,90 @@ double vec_len(Vec vec){
     return VEC_SQRT(result);  
 }
 
+Vec vec_normalize(Vec vec){
+    return vec_mul(vec, 1.0/vec_len(vec));
+}
+
+Vec vec_rotate2d(Vec vec, double deg){
+    VEC_ASSERT(vec.dim==2 && "The vector isnt 2D");
+    Vec result = vec_alloc(2);
+    Vec im = vec_normalize(vec);
+    result.data[0] = (im.data[0]*VEC_COS(DEG2RAD(deg)))-(im.data[1]*VEC_SIN(DEG2RAD(deg)));
+    result.data[1] = (im.data[0]*VEC_SIN(DEG2RAD(deg)))+(im.data[1]*VEC_COS(DEG2RAD(deg)));
+    return vec_mul(result, vec_len(vec));
+}
+
+Vec vec_rotate2d_ex(Vec vec, float x, float y, double deg){
+    VEC_ASSERT(vec.dim==2 && "The vector isnt 2D");
+    Vec p = vec_alloc(2);
+    p.data[0] = x;
+    p.data[1] = y;
+    vec = vec_sub(vec, p);
+    Vec im = vec_normalize(vec);
+    Vec result = vec_alloc(2);
+    result.data[0] = (im.data[0]*VEC_COS(DEG2RAD(deg)))-(im.data[1]*VEC_SIN(DEG2RAD(deg)));
+    result.data[1] = (im.data[0]*VEC_SIN(DEG2RAD(deg)))+(im.data[1]*VEC_COS(DEG2RAD(deg)));
+    return vec_add(vec_mul(result, vec_len(vec)), p);
+}
+
+Vec vec_rotate3d(Vec vec, double xdeg,double ydeg, double zdeg){
+    VEC_ASSERT(vec.dim==3 && "The vector isnt 3D");
+    double xrad = DEG2RAD(xdeg);
+    double yrad = DEG2RAD(ydeg);
+    double zrad = DEG2RAD(zdeg);
+    
+    double cosx = VEC_COS(xrad);
+    double cosy = VEC_COS(yrad);
+    double cosz = VEC_COS(zrad);
+    double sinx = VEC_SIN(xrad);
+    double siny = VEC_SIN(yrad);
+    double sinz = VEC_SIN(zrad);
+
+    double len = vec_len(vec);
+    vec = vec_normalize(vec);
+
+    Vec result = vec_alloc(3);
+    result.data[0] = vec.data[0]*cosx*cosy+vec.data[1]*(cosx*siny*sinz-sinx*cosz)+vec.data[2]*(cosx*siny*cosz+sinx*sinz);
+    result.data[1] = vec.data[0]*sinx*cosy+vec.data[1]*(sinx*siny*sinz+cosx*cosz)+vec.data[2]*(sinx*siny*cosz-cosx*sinz);
+    result.data[2] = vec.data[0]*(-siny)+vec.data[1]*cosy*sinz+vec.data[2]*cosy*cosz;
+    
+    return vec_mul(result, len);
+}
+
+Vec vec_rotate3d_x(Vec vec, double xdeg){
+    VEC_ASSERT(vec.dim==3 && "The vector isnt 3D");
+    Vec result = vec_alloc(3);
+    double xrad = DEG2RAD(xdeg);
+    double sinx = VEC_SIN(xdeg);
+    double cosx = VEC_COS(xdeg);
+    result.data[0] = vec.data[0];
+    result.data[1] = vec.data[1]*cosx-vec.data[2]*sinx;
+    result.data[2] = vec.data[1]*sinx+vec.data[2]*cosx;
+    return result;
+}
+
+Vec vec_rotate3d_y(Vec vec, double ydeg){
+    VEC_ASSERT(vec.dim==3 && "The vector isnt 3D");
+    Vec result = vec_alloc(3);
+    double yrad = DEG2RAD(ydeg);
+    double siny = VEC_SIN(ydeg);
+    double cosy = VEC_COS(ydeg);
+    result.data[0] = vec.data[0]*cosy+vec.data[2]*siny;
+    result.data[1] = vec.data[1];
+    result.data[2] = (-vec.data[0]*siny)+vec.data[2]*cosy;
+    return result;
+}
+Vec vec_rotate3d_z(Vec vec, double zdeg){
+    VEC_ASSERT(vec.dim==3 && "The vector isnt 3D");
+    Vec result = vec_alloc(3);
+    double zrad = DEG2RAD(zdeg);
+    double sinz = VEC_SIN(zdeg);
+    double cosz = VEC_COS(zdeg);
+    result.data[0] = vec.data[0]*cosz-vec.data[1]*sinz;
+    result.data[1] = vec.data[0]*sinz+vec.data[1]*cosz;
+    result.data[2] = vec.data[2];
+    return result;
+}
 
 #endif // VEC_IMPL
 
